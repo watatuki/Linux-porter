f9170cb0c23cc02bf17b3edf89760f014ee73ceb
Mark Brown <broonie@linaro.org>
2013-10-09 12:28:52 +0100

    regmap: Simplify the initiation of async I/O
    Rather than passing a flag around through the entire call stack store it
    in the regmap struct and read it when required. This minimises the
    visibility of the feature through the API, minimising the code updates
    needed to use it more widely.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit 0a8198094da895c8d5db95812fe9de7027d808e4)
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- drivers/base/regmap/internal.h
+++ drivers/base/regmap/internal.h
@@ -62,6 +62,7 @@ struct regmap {
 	void *bus_context;
 	const char *name;
 
+	bool async;
 	spinlock_t async_lock;
 	wait_queue_head_t async_waitq;
 	struct list_head async_list;
@@ -231,7 +232,7 @@ static inline bool regcache_reg_present(struct regmap *map, unsigned int reg)
 }
 
 int _regmap_raw_write(struct regmap *map, unsigned int reg,
-		      const void *val, size_t val_len, bool async);
+		      const void *val, size_t val_len);
 
 void regmap_async_complete_cb(struct regmap_async *async, int ret);
 
