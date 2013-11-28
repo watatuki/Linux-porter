905ab5881d2bddce543e58d98de62c9ac36531ab
Chew, Chiau Ee <chiau.ee.chew@intel.com>
2013-11-29 02:13:11 +0800

    spi/pxa2xx: Restore private register bits.
    The Intel LPSS SPI private register bits have to be restored
    when system resume from S3 suspend.
    
    Signed-off-by: Chew, Chiau Ee <chiau.ee.chew@intel.com>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit c50325f7bcb8a3ceaacb9dbc41180b1cbbae7b5e)
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/spi/spi-pxa2xx.c
+++ drivers/spi/spi-pxa2xx.c
@@ -1325,6 +1325,9 @@ static int pxa2xx_spi_resume(struct device *dev)
 	if (!pm_runtime_suspended(dev))
 		clk_prepare_enable(ssp->clk);
 
+	/* Restore LPSS private register bits */
+	lpss_ssp_setup(drv_data);
+
 	/* Start the queue running */
 	status = spi_master_resume(drv_data->master);
 	if (status != 0) {
