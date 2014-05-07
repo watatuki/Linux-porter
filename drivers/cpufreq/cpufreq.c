337c127b215fa63c303c6a4969993a6b9228f659
Stratos Karafotis <stratosk@semaphore.gr>
2014-05-07 19:33:33 +0300

    cpufreq: Fix build error on some platforms that use cpufreq_for_each_*
    On platforms that use cpufreq_for_each_* macros, build fails if
    CONFIG_CPU_FREQ=n, e.g. ARM/shmobile/koelsch/non-multiplatform:
    
    drivers/built-in.o: In function `clk_round_parent':
    clkdev.c:(.text+0xcf168): undefined reference to `cpufreq_next_valid'
    drivers/built-in.o: In function `clk_rate_table_find':
    clkdev.c:(.text+0xcf820): undefined reference to `cpufreq_next_valid'
    make[3]: *** [vmlinux] Error 1
    
    Fix this making cpufreq_next_valid function inline and move it to
    cpufreq.h.
    
    Fixes: 27e289dce297 (cpufreq: Introduce macros for cpufreq_frequency_table iteration)
    Reported-and-tested-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 5eeaf1f1897372590105f155c6a7110b3fa36aef)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq.c
    	include/linux/cpufreq.h
    
    (cherry picked from commit 256fae0b1078e828c308f68da83fd6e92f335ef9)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -238,17 +238,6 @@ static void cpufreq_cpu_put_sysfs(struct cpufreq_policy *data)
 	__cpufreq_cpu_put(data, true);
 }
 
-bool cpufreq_next_valid(struct cpufreq_frequency_table **pos)
-{
-	while ((*pos)->frequency != CPUFREQ_TABLE_END)
-		if ((*pos)->frequency != CPUFREQ_ENTRY_INVALID)
-			return true;
-		else
-			(*pos)++;
-	return false;
-}
-EXPORT_SYMBOL_GPL(cpufreq_next_valid);
-
 /*********************************************************************
  *            EXTERNALLY AFFECTING FREQUENCY CHANGES                 *
  *********************************************************************/
