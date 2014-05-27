c8fa6c4af0897c46a3b43fad393158a84cac0a6a
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2014-05-27 14:41:05 +0200

    v4l: Support extending the v4l2_pix_format structure
    The v4l2_pix_format structure has no reserved field. It is embedded in
    the v4l2_framebuffer structure which has no reserved fields either, and
    in the v4l2_format structure which has reserved fields that were not
    previously required to be zeroed out by applications.
    
    To allow extending v4l2_pix_format, inline it in the v4l2_framebuffer
    structure, and use the priv field as a magic value to indicate that the
    application has set all v4l2_pix_format extended fields and zeroed all
    reserved fields following the v4l2_pix_format field in the v4l2_format
    structure.
    
    The availability of this API extension is reported to userspace through
    the new V4L2_CAP_EXT_PIX_FORMAT capability flag. Just checking that the
    priv field is still set to the magic value at [GS]_FMT return wouldn't
    be enough, as older kernels don't zero the priv field on return.
    
    To simplify the internal API towards drivers zero the extended fields
    and set the priv field to the magic value for applications not aware of
    the extensions.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    (cherry picked from commit af7553db487e1dbcd069d1bb9426fc8c591ea1f4)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    
    Conflicts:
    	Documentation/DocBook/media/v4l/v4l2.xml
    	Documentation/DocBook/media/v4l/vidioc-querycap.xml
    	drivers/media/pci/saa7134/saa7134-empress.c
    	drivers/media/pci/saa7134/saa7134-video.c
    	drivers/media/platform/davinci/vpif_display.c
    	drivers/media/platform/mem2mem_testdev.c
    	drivers/media/usb/gspca/gspca.c
    	include/uapi/linux/videodev2.h
    
    (cherry picked from commit f68fdb42e51d74b41aa9cd1fb7435c7d84225373)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -264,6 +264,8 @@ struct v4l2_capability {
 #define V4L2_CAP_RADIO			0x00040000  /* is a radio device */
 #define V4L2_CAP_MODULATOR		0x00080000  /* has a modulator */
 
+#define V4L2_CAP_EXT_PIX_FORMAT		0x00200000  /* Supports the extended pixel format */
+
 #define V4L2_CAP_READWRITE              0x01000000  /* read/write systemcalls */
 #define V4L2_CAP_ASYNCIO                0x02000000  /* async I/O */
 #define V4L2_CAP_STREAMING              0x04000000  /* streaming I/O ioctls */
@@ -445,6 +447,9 @@ struct v4l2_pix_format {
 
 /* Flags */
 #define V4L2_PIX_FMT_FLAG_PREMUL_ALPHA	0x00000001
+
+/* priv field value to indicates that subsequent fields are valid. */
+#define V4L2_PIX_FMT_PRIV_MAGIC		0xdeadbeef
 
 /*
  *	F O R M A T   E N U M E R A T I O N
@@ -739,7 +744,16 @@ struct v4l2_framebuffer {
 /* FIXME: in theory we should pass something like PCI device + memory
  * region + offset instead of some physical address */
 	void                    *base;
-	struct v4l2_pix_format	fmt;
+	struct {
+		__u32		width;
+		__u32		height;
+		__u32		pixelformat;
+		__u32		field;		/* enum v4l2_field */
+		__u32		bytesperline;	/* for padding, zero if unused */
+		__u32		sizeimage;
+		__u32		colorspace;	/* enum v4l2_colorspace */
+		__u32		priv;		/* private data, depends on pixelformat */
+	} fmt;
 };
 /*  Flags for the 'capability' field. Read only */
 #define V4L2_FBUF_CAP_EXTERNOVERLAY	0x0001
