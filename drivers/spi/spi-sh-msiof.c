fd7193203a9fdaefce8283ea454c7f9b16d30a5a
Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>
2014-08-01 10:20:35 +0900

    spi: sh-msiof: Add r8a7793 support
    The device tree probing for R-Car M2N (r8a7793) is added.
    
    Signed-off-by: Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -964,6 +964,7 @@ static const struct of_device_id sh_msiof_match[] = {
 	{ .compatible = "renesas,sh-mobile-msiof", .data = &sh_data },
 	{ .compatible = "renesas,msiof-r8a7790",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7791",   .data = &r8a779x_data },
+	{ .compatible = "renesas,msiof-r8a7793",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7794",   .data = &r8a779x_data },
 	{},
 };
@@ -1255,6 +1256,7 @@ static struct platform_device_id spi_driver_ids[] = {
 	{ "spi_sh_msiof",	(kernel_ulong_t)&sh_data },
 	{ "spi_r8a7790_msiof",	(kernel_ulong_t)&r8a779x_data },
 	{ "spi_r8a7791_msiof",	(kernel_ulong_t)&r8a779x_data },
+	{ "spi_r8a7793_msiof",	(kernel_ulong_t)&r8a779x_data },
 	{ "spi_r8a7794_msiof",	(kernel_ulong_t)&r8a779x_data },
 	{},
 };
