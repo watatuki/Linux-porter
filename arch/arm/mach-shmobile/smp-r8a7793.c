20fb32704aa2103da10357df136b782dce668aa8
Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>
2014-09-10 15:08:04 +0900

    ARM: shmobile: Correct number of CPU cores for M2N
    R-Car M2N only have 2 CPU cores and don't have CA7.
    So, fix a incorrect declaration.
    
    Signed-off-by: Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>

--- arch/arm/mach-shmobile/smp-r8a7793.c
+++ arch/arm/mach-shmobile/smp-r8a7793.c
@@ -31,12 +31,8 @@
 static struct rcar_apmu_config r8a7793_apmu_config[] = {
 	{
 		.iomem = DEFINE_RES_MEM(0xe6152000, 0x88),
-		.cpus = { 0, 1, 2, 3 },
+		.cpus = { 0, 1 },
 	},
-	{
-		.iomem = DEFINE_RES_MEM(0xe6151000, 0x88),
-		.cpus = { 0x100, 0x0101, 0x102, 0x103 },
-	}
 };
 
 static void __init r8a7793_smp_prepare_cpus(unsigned int max_cpus)
