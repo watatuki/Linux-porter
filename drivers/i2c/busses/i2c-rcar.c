a11c6c1f466ad941fbbf5fec0129b2a46718f951
Ryo Kataoka <ryo.kataoka.wt@renesas.com>
2014-11-05 13:19:34 +0900

    Revert "i2c: rcar: Avoid auto restart by HW after Nack"
    This reverts commit 599f53e8706acc7bdfd78c48ddb2009c70c9e8be.
    
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- drivers/i2c/busses/i2c-rcar.c
+++ drivers/i2c/busses/i2c-rcar.c
@@ -1,8 +1,6 @@
 /*
  *  drivers/i2c/busses/i2c-rcar.c
  *
- * Copyright (C) 2013-2014 Renesas Electronics Corporation
- *
  * Copyright (C) 2012 Renesas Solutions Corp.
  * Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
  *
@@ -103,7 +101,6 @@ enum {
 #define ID_DONE		(1 << 2)
 #define ID_ARBLOST	(1 << 3)
 #define ID_NACK		(1 << 4)
-#define ID_FIRST_MSG	(1 << 5)
 
 enum rcar_i2c_type {
 	I2C_RCAR_GEN1,
@@ -130,7 +127,6 @@ struct rcar_i2c_priv {
 #define rcar_i2c_is_recv(p)		((p)->msg->flags & I2C_M_RD)
 
 #define rcar_i2c_flags_set(p, f)	((p)->flags |= (f))
-#define rcar_i2c_flags_clr(p, f)	((p)->flags &= ~(f))
 #define rcar_i2c_flags_has(p, f)	((p)->flags & (f))
 
 #define LOOP_TIMEOUT	1024
@@ -344,15 +340,6 @@ static int rcar_i2c_recv(struct rcar_i2c_priv *priv)
 	rcar_i2c_set_addr(priv, 1);
 	rcar_i2c_status_clear(priv);
 	rcar_i2c_bus_phase(priv, RCAR_BUS_PHASE_ADDR);
-
-	if (rcar_i2c_flags_has(priv, ID_FIRST_MSG)) {	/* start */
-		/* get ESG bit clear timing */
-		rcar_i2c_read(priv, ICMCR);
-		rcar_i2c_flags_clr(priv, ID_FIRST_MSG);
-		/* clear ESG bit for avoiding auto restart by H/W */
-		rcar_i2c_bus_phase(priv, RCAR_BUS_PHASE_DATA);
-	}
-
 	rcar_i2c_irq_mask(priv, RCAR_IRQ_OPEN_FOR_RECV);
 
 	return 0;
@@ -372,15 +359,6 @@ static int rcar_i2c_send(struct rcar_i2c_priv *priv)
 	rcar_i2c_set_addr(priv, 0);
 	rcar_i2c_status_clear(priv);
 	rcar_i2c_bus_phase(priv, RCAR_BUS_PHASE_ADDR);
-
-	if (rcar_i2c_flags_has(priv, ID_FIRST_MSG)) {	/* start */
-		/* get ESG bit clear timing */
-		rcar_i2c_read(priv, ICMCR);
-		rcar_i2c_flags_clr(priv, ID_FIRST_MSG);
-		/* clear ESG bit for avoiding auto restart by H/W */
-		rcar_i2c_bus_phase(priv, RCAR_BUS_PHASE_DATA);
-	}
-
 	rcar_i2c_irq_mask(priv, RCAR_IRQ_OPEN_FOR_SEND);
 
 	return 0;
@@ -599,8 +577,6 @@ static int rcar_i2c_master_xfer(struct i2c_adapter *adap,
 		priv->msg	= &msgs[i];
 		priv->pos	= 0;
 		priv->flags	= 0;
-		if (i == 0)
-			rcar_i2c_flags_set(priv, ID_FIRST_MSG);
 		if (priv->msg == &msgs[num - 1])
 			rcar_i2c_flags_set(priv, ID_LAST_MSG);
 
