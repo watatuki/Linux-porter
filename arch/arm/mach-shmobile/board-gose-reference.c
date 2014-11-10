0dd8d07a1809ea5221cb8d69eb7e36bd151d0b16
Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
2014-11-10 13:12:03 +0900

    ARM: shmobile: gose-reference: Add gpio_vbus and wakeup for usb vbus detection
    This patch adds gpio_vbus and wakeup for usb vbus detection.
    The value of gpio_vbus is USB0_OVC(GP-7-24).
    
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>

--- arch/arm/mach-shmobile/board-gose-reference.c
+++ arch/arm/mach-shmobile/board-gose-reference.c
@@ -678,6 +678,8 @@ static const struct rcar_gen2_phy_platform_data usbhs_phy_pdata __initconst = {
 	.chan0_pci = 1,	/* Channel 0 is PCI USB */
 #endif
 	.chan2_pci = 1, /* Channel 2 is PCI USB host */
+	.gpio_vbus = 798,
+	.wakeup = true,
 };
 
 /* VIN */
