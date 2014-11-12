d5afd04585e9214db354f2e8df7d25c6276e6f36
Phil Edworthy <phil.edworthy@renesas.com>
2014-11-12 10:06:49 +0000

    ARM: shmobile: r8a7791: Remove Audio SSI/SRC device code
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>

--- arch/arm/mach-shmobile/setup-r8a7791.c
+++ arch/arm/mach-shmobile/setup-r8a7791.c
@@ -22,7 +22,6 @@
 #include <linux/irq.h>
 #include <linux/kernel.h>
 #include <linux/of_platform.h>
-#include <linux/platform_data/dma-rcar-audmapp.h>
 #include <linux/platform_data/gpio-rcar.h>
 #include <linux/platform_data/irq-renesas-irqc.h>
 #include <linux/serial_sci.h>
@@ -36,177 +35,6 @@
 #include "irqs.h"
 #include "r8a7791.h"
 #include "rcar-gen2.h"
-
-/* Audio-DMAC */
-#define AUDIO_DMAC_SLAVE(_id, _addr, toffset, roffset, t, r)	\
-{								\
-	.slave_id	= AUDIO_DMAC_SLAVE_## _id ##_TX,	\
-	.addr		= _addr + toffset,			\
-	.chcr		= CHCR_TX(XMIT_SZ_32BIT),		\
-	.mid_rid	= t,					\
-}, {								\
-	.slave_id	= AUDIO_DMAC_SLAVE_## _id ##_RX,	\
-	.addr		= _addr + roffset,			\
-	.chcr		= CHCR_RX(XMIT_SZ_32BIT),		\
-	.mid_rid	= r,					\
-}
-
-#define AUDIO_DMAC_SCU_SLAVE(_id, _addr, t, r)			\
-	AUDIO_DMAC_SLAVE(SCU##_id, _addr, 0x0, 0x4000, t, r)
-
-#define AUDIO_DMAC_SSI_SLAVE(_id, _addr, t, r)			\
-	AUDIO_DMAC_SLAVE(SSI##_id, _addr, 0x8, 0xc, t, r)
-
-#define AUDIO_DMAC_SSIU_SLAVE(_id, _addr, t, r)		\
-	AUDIO_DMAC_SLAVE(SSIU##_id, _addr, 0, 0, t, r)
-
-#define AUDIO_DMAC_CMD_SLAVE(_id, _addr, t)			\
-{								\
-	.slave_id	= AUDIO_DMAC_SLAVE_CMD## _id ##_TO_MEM,	\
-	.addr		= _addr,				\
-	.chcr		= CHCR_RX(XMIT_SZ_32BIT),		\
-	.mid_rid	= t,					\
-}
-
-static const struct sh_dmae_slave_config r8a7791_audio_dmac_slaves[] = {
-	AUDIO_DMAC_SCU_SLAVE(0, 0, 0x85, 0x9a),
-	AUDIO_DMAC_SCU_SLAVE(1, 0, 0x87, 0x9c),
-	AUDIO_DMAC_SCU_SLAVE(2, 0, 0x89, 0x9e),
-	AUDIO_DMAC_SCU_SLAVE(3, 0, 0x8b, 0xa0),
-	AUDIO_DMAC_SCU_SLAVE(4, 0, 0x8d, 0xb0),
-	AUDIO_DMAC_SCU_SLAVE(5, 0, 0x8f, 0xb2),
-	AUDIO_DMAC_SCU_SLAVE(6, 0, 0x91, 0xb4),
-	AUDIO_DMAC_SCU_SLAVE(7, 0, 0x93, 0xb6),
-	AUDIO_DMAC_SCU_SLAVE(8, 0, 0x95, 0xb8),
-	AUDIO_DMAC_SCU_SLAVE(9, 0, 0x97, 0xba),
-	AUDIO_DMAC_SSI_SLAVE(0, 0, 0x01, 0x02),
-	AUDIO_DMAC_SSI_SLAVE(1, 0, 0x03, 0x04),
-	AUDIO_DMAC_SSI_SLAVE(2, 0, 0x05, 0x06),
-	AUDIO_DMAC_SSI_SLAVE(3, 0, 0x07, 0x08),
-	AUDIO_DMAC_SSI_SLAVE(4, 0, 0x09, 0x0a),
-	AUDIO_DMAC_SSI_SLAVE(5, 0, 0x0b, 0x0c),
-	AUDIO_DMAC_SSI_SLAVE(6, 0, 0x0d, 0x0e),
-	AUDIO_DMAC_SSI_SLAVE(7, 0, 0x0f, 0x10),
-	AUDIO_DMAC_SSI_SLAVE(8, 0, 0x11, 0x12),
-	AUDIO_DMAC_SSI_SLAVE(9, 0, 0x13, 0x14),
-	AUDIO_DMAC_SSIU_SLAVE(0, 0, 0x15, 0x16),
-	AUDIO_DMAC_SSIU_SLAVE(1, 0, 0x49, 0x4a),
-	AUDIO_DMAC_SSIU_SLAVE(2, 0, 0x63, 0x64),
-	AUDIO_DMAC_SSIU_SLAVE(3, 0, 0x6f, 0x70),
-	AUDIO_DMAC_SSIU_SLAVE(4, 0, 0x71, 0x72),
-	AUDIO_DMAC_SSIU_SLAVE(5, 0, 0x73, 0x74),
-	AUDIO_DMAC_SSIU_SLAVE(6, 0, 0x75, 0x76),
-	AUDIO_DMAC_SSIU_SLAVE(7, 0, 0x79, 0x7a),
-	AUDIO_DMAC_SSIU_SLAVE(8, 0, 0x7b, 0x7c),
-	AUDIO_DMAC_SSIU_SLAVE(9, 0, 0x7d, 0x7e),
-	AUDIO_DMAC_CMD_SLAVE(1, 0, 0xbe),
-};
-
-#define DMAE_CHANNEL(a, b)			\
-{						\
-	.offset		= (a) - 0x20,		\
-	.dmars		= (a) - 0x20 + 0x40,	\
-	.chclr_bit	= (b),			\
-	.chclr_offset	= 0x80 - 0x20,		\
-}
-
-static const struct sh_dmae_channel r8a7791_audio_dmac_channels[] = {
-	DMAE_CHANNEL(0x8000, 0),
-	DMAE_CHANNEL(0x8080, 1),
-	DMAE_CHANNEL(0x8100, 2),
-	DMAE_CHANNEL(0x8180, 3),
-	DMAE_CHANNEL(0x8200, 4),
-	DMAE_CHANNEL(0x8280, 5),
-	DMAE_CHANNEL(0x8300, 6),
-	DMAE_CHANNEL(0x8380, 7),
-	DMAE_CHANNEL(0x8400, 8),
-	DMAE_CHANNEL(0x8480, 9),
-	DMAE_CHANNEL(0x8500, 10),
-	DMAE_CHANNEL(0x8580, 11),
-	DMAE_CHANNEL(0x8600, 12),
-};
-
-static struct sh_dmae_pdata r8a7791_audio_dmac_platform_data = {
-	.slave		= r8a7791_audio_dmac_slaves,
-	.slave_num	= ARRAY_SIZE(r8a7791_audio_dmac_slaves),
-	.channel	= r8a7791_audio_dmac_channels,
-	.channel_num	= ARRAY_SIZE(r8a7791_audio_dmac_channels),
-	.ts_low_shift	= TS_LOW_SHIFT,
-	.ts_low_mask	= TS_LOW_BIT << TS_LOW_SHIFT,
-	.ts_high_shift	= TS_HI_SHIFT,
-	.ts_high_mask	= TS_HI_BIT << TS_HI_SHIFT,
-	.ts_shift	= dma_ts_shift,
-	.ts_shift_num	= ARRAY_SIZE(dma_ts_shift),
-	.dmaor_init	= DMAOR_DME,
-	.chclr_present	= 1,
-	.chclr_bitwise	= 1,
-};
-
-static struct resource r8a7791_audio_dmac_resources[] = {
-	/* Channel registers and DMAOR for low */
-	DEFINE_RES_MEM(0xec700020, 0x8663 - 0x20),
-	DEFINE_RES_IRQ(gic_spi(346)),
-	DEFINE_RES_NAMED(gic_spi(320), 13, NULL, IORESOURCE_IRQ),
-
-	/* Channel registers and DMAOR for hi */
-	DEFINE_RES_MEM(0xec720020, 0x8663 - 0x20), /* hi */
-	DEFINE_RES_IRQ(gic_spi(347)),
-	DEFINE_RES_NAMED(gic_spi(333), 13, NULL, IORESOURCE_IRQ),
-};
-
-#define r8a7791_register_audio_dmac(id)				\
-	platform_device_register_resndata(			\
-		&platform_bus, "sh-dma-engine", id,		\
-		&r8a7791_audio_dmac_resources[id * 3],	3,	\
-		&r8a7791_audio_dmac_platform_data,		\
-		sizeof(r8a7791_audio_dmac_platform_data))
-
-/* Audio DMAC peri peri */
-#define AUDMAPP_CFG(id, _src, _dst, _chcr)				\
-	{ .slave_id = AUDIOPP_DMAC_SLAVE_##id,				\
-			.src = _src, .dst = _dst, .chcr = _chcr << 16}
-
-static struct audmapp_slave_config r8a7791_audmapp_slaves[] = {
-	AUDMAPP_CFG(SCU0_TO_SSI0, 0, 0, 0x2d00),
-	AUDMAPP_CFG(SCU1_TO_SSI1, 0, 0, 0x2e04),
-	AUDMAPP_CFG(SCU2_TO_SSI2, 0, 0, 0x2f08),
-	AUDMAPP_CFG(SCU3_TO_SSI3, 0, 0, 0x300c),
-	AUDMAPP_CFG(SCU4_TO_SSI4, 0, 0, 0x310d),
-	AUDMAPP_CFG(SCU5_TO_SSI5, 0, 0, 0x320e),
-	AUDMAPP_CFG(SCU6_TO_SSI6, 0, 0, 0x330f),
-	AUDMAPP_CFG(SCU7_TO_SSI7, 0, 0, 0x3410),
-	AUDMAPP_CFG(SCU8_TO_SSI8, 0, 0, 0x3511),
-	AUDMAPP_CFG(SCU9_TO_SSI9, 0, 0, 0x3612),
-	AUDMAPP_CFG(CMD0_TO_SSI0, 0, 0, 0x3700),
-
-	AUDMAPP_CFG(SSI0_TO_SCU0, 0, 0, 0x002d),
-	AUDMAPP_CFG(SSI1_TO_SCU1, 0, 0, 0x042e),
-	AUDMAPP_CFG(SSI2_TO_SCU2, 0, 0, 0x082f),
-	AUDMAPP_CFG(SSI3_TO_SCU3, 0, 0, 0x0c30),
-	AUDMAPP_CFG(SSI4_TO_SCU4, 0, 0, 0x0d31),
-	AUDMAPP_CFG(SSI5_TO_SCU5, 0, 0, 0x0e32),
-	AUDMAPP_CFG(SSI6_TO_SCU6, 0, 0, 0x0f33),
-	AUDMAPP_CFG(SSI7_TO_SCU7, 0, 0, 0x1034),
-	AUDMAPP_CFG(SSI8_TO_SCU8, 0, 0, 0x1135),
-	AUDMAPP_CFG(SSI9_TO_SCU9, 0, 0, 0x1236),
-};
-
-struct audmapp_pdata r8a7791_audmapp_platform_data = {
-	.slave		= r8a7791_audmapp_slaves,
-	.slave_num	= ARRAY_SIZE(r8a7791_audmapp_slaves),
-};
-
-static const struct resource r8a7791_audmapp_resources[] __initconst = {
-	DEFINE_RES_MEM(0xec740000, 0x200),
-};
-
-#define r8a7791_register_audmapp()				\
-	platform_device_register_resndata(&platform_bus,	\
-		"rcar-audmapp-engine", -1,			\
-		r8a7791_audmapp_resources,			\
-		ARRAY_SIZE(r8a7791_audmapp_resources),		\
-		&r8a7791_audmapp_platform_data,			\
-		sizeof(r8a7791_audmapp_platform_data));
 
 static const struct resource pfc_resources[] __initconst = {
 	DEFINE_RES_MEM(0xe6060000, 0x250),
@@ -430,9 +258,6 @@ void __init r8a7791_add_dt_devices(void)
 	r8a7791_pm_init();
 	r8a7791_init_pm_domains();
 	r8a7791_register_cmt(00);
-	r8a7791_register_audio_dmac(0);
-	r8a7791_register_audio_dmac(1);
-	r8a7791_register_audmapp();
 	r8a7791_register_pvrsrvkm();
 	r8a7791_register_ssp();
 }
