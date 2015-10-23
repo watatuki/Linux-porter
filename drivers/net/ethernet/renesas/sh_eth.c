8c7f957cf4a19fc947511ccddabc507c14156547
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2015-10-24 00:46:40 +0300

    sh_eth: fix RX buffer size calculation
    The RX buffer size calulation failed to account for the length granularity
    (which is now 32 bytes)...
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit cb3685958dd4c46d7646d244063ea3ec8adf3618)
    
    Conflicts:
    
    	drivers/net/ethernet/renesas/sh_eth.c
    
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -1123,7 +1123,7 @@ static void sh_eth_ring_format(struct net_device *ndev)
 	struct sh_eth_txdesc *txdesc = NULL;
 	int rx_ringsize = sizeof(*rxdesc) * mdp->num_rx_ring;
 	int tx_ringsize = sizeof(*txdesc) * mdp->num_tx_ring;
-	int skbuff_size = mdp->rx_buf_sz + SH_ETH_RX_ALIGN - 1;
+	int skbuff_size = mdp->rx_buf_sz + SH_ETH_RX_ALIGN + 32 - 1;
 
 	mdp->cur_rx = 0;
 	mdp->cur_tx = 0;
@@ -1409,7 +1409,7 @@ static int sh_eth_rx(struct net_device *ndev, u32 intr_status, int *quota)
 	struct sk_buff *skb;
 	u16 pkt_len = 0;
 	u32 desc_status;
-	int skbuff_size = mdp->rx_buf_sz + SH_ETH_RX_ALIGN - 1;
+	int skbuff_size = mdp->rx_buf_sz + SH_ETH_RX_ALIGN + 32 - 1;
 
 	if (quota)
 		limit = boguscnt = min(boguscnt, *quota);
