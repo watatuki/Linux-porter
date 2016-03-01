49646b780003496c8257e5ae9d23cba95b091470
Hiroki Negishi <hiroki.negishi.bx@bp.renesas.com>
2016-03-01 20:47:25 +0900

    v4l: vsp1: Fix compile error in fix of Suspend to RAM
    pdata is a pointer of struct vsp1_platform_data.
    But reference to pdata is incorrect as follows:
      vsp1->pdata.wpf_count
    This fixes reference to pdata.
    
    Signed-off-by: Hiroki Negishi <hiroki.negishi.bx@bp.renesas.com>

--- drivers/media/platform/vsp1/vsp1_video.c
+++ drivers/media/platform/vsp1/vsp1_video.c
@@ -856,7 +856,7 @@ void vsp1_pipelines_suspend(struct vsp1_device *vsp1)
 	 * pipelines twice, first to set them all to the stopping state, and then
 	 * to wait for the stop to complete.
 	 */
-	for (i = 0; i < vsp1->pdata.wpf_count; ++i) {
+	for (i = 0; i < vsp1->pdata->wpf_count; ++i) {
 		struct vsp1_rwpf *wpf = vsp1->wpf[i];
 		struct vsp1_pipeline *pipe;
 
@@ -873,7 +873,7 @@ void vsp1_pipelines_suspend(struct vsp1_device *vsp1)
 		spin_unlock_irqrestore(&pipe->irqlock, flags);
 	}
 
-	for (i = 0; i < vsp1->pdata.wpf_count; ++i) {
+	for (i = 0; i < vsp1->pdata->wpf_count; ++i) {
 		struct vsp1_rwpf *wpf = vsp1->wpf[i];
 		struct vsp1_pipeline *pipe;
 
@@ -898,7 +898,7 @@ void vsp1_pipelines_resume(struct vsp1_device *vsp1)
 	unsigned int i;
 
 	/* Resume pipeline all running pipelines. */
-	for (i = 0; i < vsp1->pdata.wpf_count; ++i) {
+	for (i = 0; i < vsp1->pdata->wpf_count; ++i) {
 		struct vsp1_rwpf *wpf = vsp1->wpf[i];
 		struct vsp1_pipeline *pipe;
 
